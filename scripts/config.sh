e754961b6913d5dff6e2827ffe3bc9081389d7ef
Roy Layer <rlayer666@yahoo.com>
2014-03-30 14:57:10 -0500

    intial build scripts

---
+++ scripts/config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git config --global user.email "rlayer666@yahoo.com"
+git config --global user.name "Roy Layer"
+
+exit
+
---
+++ scripts/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+
+sudo apt-get update && sudo apt-get dist-upgrade && sudo reboot
+
+exit
+
